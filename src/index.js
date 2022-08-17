527d6cc19e12564a988db4ab973b8706ccabbda1
George <georgea.fotheringham@gmail.com>
2022-08-16 21:46:15 -0400

    adds base css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { pageLoad } from './page-load.js';
 import './style.css';
 
 const content = document.querySelector('div')
-const contactTab = document.createElement('button','.contact');
+const contactTab = document.createElement('button');
 contactTab.addEventListener('click', switchTabs);
 content.appendChild(contactTab);
 
