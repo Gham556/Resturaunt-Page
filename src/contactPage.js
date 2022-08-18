290185e88d53977ce6790993309b016fff1f3993
George <georgea.fotheringham@gmail.com>
2022-08-18 15:24:25 -0400

    adds contact page content

--- src/contactPage.js
+++ src/contactPage.js
@@ -8,13 +8,39 @@ export function contactPageLoad() {
     const content = document.querySelector('div')
     const switchContainer = document.querySelector('.switchContainer')
     const babyCard = document.createElement('div');
+    const babycardText = document.createElement('div');
 
+    const babyName = document.createElement('div');
+    const babyDetails = document.createElement('div');
+
+    const babyJob = document.createElement('div');
+    const babyNum = document.createElement('div');
+    const babyEmail = document.createElement('div');
+    
     image.classList.add('logo');
     babyCard.classList.add('babyCard');
+    babycardText.classList.add('babyCardText');
+    babyName.classList.add('babyName');
+    babyDetails.classList.add('babyDetails');
 
     image.src = babies;
     profilPic.src = babyGirl;
 
-    switchContainer.appendChild(babyCard)
-    babyCard.appendChild(profilPic)
+    babyName.textContent = 'BABY';
+    babyJob.textContent = 'Chef';
+    babyNum.textContent = '111-222-3333';
+    babyEmail.textContent = 'whoGaveABabyAnEmail@email.com';
+
+
+    switchContainer.appendChild(babyCard);
+    
+    babyCard.appendChild(profilPic);
+    babyCard.appendChild(babycardText);
+
+    babycardText.appendChild(babyName);
+    babycardText.appendChild(babyDetails);
+
+    babyDetails.appendChild(babyJob);
+    babyDetails.appendChild(babyNum);
+    babyDetails.appendChild(babyEmail);
 }
